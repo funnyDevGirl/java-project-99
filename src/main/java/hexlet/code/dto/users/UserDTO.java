39b6013ae77d42f1b79b9a48954d4986d2f2025f
alina tarasova <alishka.tarasova@gmail.com>
2024-04-13 11:25:32 +0300

    Added dto/ with UserDTO.java, UserUpdateDTO.java, UserCreateDTO.java

---
+++ src/main/java/hexlet/code/dto/users/UserDTO.java
@@ -0,0 +1,14 @@
+package hexlet.code.dto.users;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String createdAt;
+}
