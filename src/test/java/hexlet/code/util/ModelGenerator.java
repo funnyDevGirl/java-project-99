33479622544652f7a1d2ed46c64de3deb07d8fd9
alina tarasova <alishka.tarasova@gmail.com>
2024-04-13 11:38:44 +0300

    Added Generator for Tests: ModelGenerator.java

---
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -0,0 +1,34 @@
+package hexlet.code.util;
+
+
+import hexlet.code.model.User;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.instancio.Model;
+import org.instancio.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Getter
+@Component
+public class ModelGenerator {
+    private Model<User> userModel;
+    @Autowired
+    private Faker faker;
+
+    @PostConstruct
+    private void init() {
+        faker = new Faker(Locale.US);
+        userModel = Instancio.of(User.class)
+                .ignore(Select.field(User::getId))
+                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
+                .supply(Select.field(User::getLastName), () -> faker.name().lastName())
+                .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
+                .supply(Select.field(User::getPasswordDigest), () -> faker.internet().password(3, 100))
+                .toModel();
+    }
+}
