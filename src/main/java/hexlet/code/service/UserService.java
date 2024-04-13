5c8ea9f770321a7b1bdc4078d996d32733f0d956
alina tarasova <alishka.tarasova@gmail.com>
2024-04-13 11:31:15 +0300

    Added service/ with UserService.java, CustomUserDetailsService.java

---
+++ src/main/java/hexlet/code/service/UserService.java
@@ -0,0 +1,56 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.users.UserCreateDTO;
+import hexlet.code.dto.users.UserDTO;
+import hexlet.code.dto.users.UserUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.UserMapper;
+import hexlet.code.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public UserDTO create(UserCreateDTO userCreateDTO) {
+        var user = userMapper.map(userCreateDTO);
+        userRepository.save(user);
+        return userMapper.map(user);
+    }
+
+    public List<UserDTO> getAll() {
+        var users = userRepository.findAll();
+        return users.stream()
+                .map(userMapper::map)
+                .toList();
+    }
+
+    public UserDTO findById(Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User With Id: " + id + " Not Found"));
+        return userMapper.map(user);
+    }
+
+    public UserDTO update(UserUpdateDTO userUpdateDTO, Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User With Id: " + id + " Not Found"));
+        userMapper.update(userUpdateDTO, user);
+        userRepository.save(user);
+        return userMapper.map(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
