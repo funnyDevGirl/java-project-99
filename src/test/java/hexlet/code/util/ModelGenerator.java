8b07202645721f29940826f1ba3693fe34481e6a
alina tarasova <alishka.tarasova@gmail.com>
2024-04-14 17:39:45 +0300

    Updated ModelGenerator: added Model TaskStatus

--- src/test/java/hexlet/code/util/ModelGenerator.java
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -1,6 +1,6 @@
 package hexlet.code.util;
 
-
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
@@ -16,19 +16,29 @@ import java.util.Locale;
 @Getter
 @Component
 public class ModelGenerator {
-    private Model<User> userModel;
+
     @Autowired
     private Faker faker;
+
+    private Model<User> userModel;
+    private Model<TaskStatus> taskStatusModel;
 
     @PostConstruct
     private void init() {
         faker = new Faker(Locale.US);
+
         userModel = Instancio.of(User.class)
                 .ignore(Select.field(User::getId))
                 .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
                 .supply(Select.field(User::getLastName), () -> faker.name().lastName())
                 .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
                 .supply(Select.field(User::getPasswordDigest), () -> faker.internet().password(3, 100))
+                .toModel();
+
+        taskStatusModel = Instancio.of(TaskStatus.class)
+                .ignore(Select.field(TaskStatus::getId))
+                .supply(Select.field(TaskStatus::getName), () -> faker.lorem().word())
+                .supply(Select.field(TaskStatus::getSlug), () -> faker.internet().slug())
                 .toModel();
     }
 }
