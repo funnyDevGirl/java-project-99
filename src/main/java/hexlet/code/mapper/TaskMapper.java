e4f877c42f597b4329fdc6a8f725ba2474d13388
alina tarasova <alishka.tarasova@gmail.com>
2024-04-15 20:43:00 +0300

    Added TaskMapper.java

---
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -0,0 +1,62 @@
+package hexlet.code.mapper;
+
+import hexlet.code.dto.tasks.TaskCreateDTO;
+import hexlet.code.dto.tasks.TaskDTO;
+import hexlet.code.dto.tasks.TaskUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.repository.TaskStatusRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Mapper(
+        uses = {JsonNullableMapper.class, ReferenceMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(source = "status", target = "taskStatus", qualifiedByName = "slugToTaskStatus")
+    @Mapping(source = "assigneeId", target = "assignee")
+    public abstract Task map(TaskCreateDTO taskCreateDTO);
+
+
+    @Mapping(source = "name", target = "title")
+    @Mapping(source = "description", target = "content")
+    @Mapping(source = "taskStatus", target = "status")
+    @Mapping(source = "assignee", target = "assigneeId")
+    public abstract TaskDTO map(Task task);
+
+
+    @Mapping(source = "title", target = "name")
+    @Mapping(source = "content", target = "description")
+    @Mapping(source = "assigneeId", target = "assignee")
+    @Mapping(source = "status", target = "taskStatus", qualifiedByName = "slugToTaskStatus")
+    public abstract void update(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task);
+
+
+    @Named("slugToTaskStatus")
+    public TaskStatus slugToTaskStatus(String slug) {
+        return taskStatusRepository.findBySlug(slug).orElseThrow(
+                () -> new ResourceNotFoundException("TaskStatus with slug " + slug + " not found"));
+    }
+
+//    @Named("assigneeIdToUser")
+//    public User assigneeIdToUser(long id) {
+//        return id == null ? null : new User(id);
+//    }
+}
