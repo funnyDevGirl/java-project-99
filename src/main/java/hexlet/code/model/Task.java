469ed80df4ec1f4d6f75abf6c6b5bcbb9671778a
alina tarasova <alishka.tarasova@gmail.com>
2024-04-15 20:40:58 +0300

    Added Task.java

---
+++ src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,51 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.CascadeType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class Task implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Size(min = 1)
+    private String name;
+
+    private Integer index;
+
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TaskStatus taskStatus;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    private User assignee;
+
+    @CreatedDate
+    private LocalDate createdAt;
+}
