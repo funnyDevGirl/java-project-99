11296131f593af20fbc9cf8ffb5f594439190329
alina tarasova <alishka.tarasova@gmail.com>
2024-04-19 15:51:37 +0300

    Updated TaskService.java

--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -5,7 +5,10 @@ import hexlet.code.dto.tasks.TaskDTO;
 import hexlet.code.dto.tasks.TaskUpdateDTO;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.TaskMapper;
+import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,10 +23,20 @@ public class TaskService {
     private TaskRepository taskRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
     private TaskMapper taskMapper;
 
     public TaskDTO create(TaskCreateDTO taskCreateDTO) {
         var task = taskMapper.map(taskCreateDTO);
+
         taskRepository.save(task);
         return taskMapper.map(task);
     }
@@ -45,6 +58,7 @@ public class TaskService {
         var task = taskRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Task With Id: " + id + " Not Found"));
         taskMapper.update(taskUpdateDTO, task);
+
         taskRepository.save(task);
         return taskMapper.map(task);
     }
