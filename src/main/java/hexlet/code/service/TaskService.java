c000df1c763e7a1ccd37f649a79d04c0fc365a60
alina tarasova <alishka.tarasova@gmail.com>
2024-04-27 14:35:38 +0300

    Updated TaskService.java

--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -6,11 +6,20 @@ import hexlet.code.dto.tasks.TaskFilterDTO;
 import hexlet.code.dto.tasks.TaskUpdateDTO;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.TaskMapper;
+import hexlet.code.model.Label;
+import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import hexlet.code.specification.TaskSpecification;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -20,13 +29,34 @@ public class TaskService {
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
     private final TaskSpecification taskSpecification;
+    private final UserRepository userRepository;
+    private final TaskStatusRepository taskStatusRepository;
+    private final LabelRepository labelRepository;
 
 
     public TaskDTO create(TaskCreateDTO taskCreateDTO) {
-        var task = taskMapper.map(taskCreateDTO);
 
-        taskRepository.save(task);
-        return taskMapper.map(task);
+        try {
+            var task = taskMapper.map(taskCreateDTO);
+
+            //записываю таску автору и автора таске:
+            var assignee = task.getAssignee();
+            assignee.addTask(task);
+
+            //записываю таску статусу и статус таске:
+            var status = task.getTaskStatus();
+            status.addTask(task);
+
+            //записываю таску в метку и метку в таску:
+            var labels = task.getLabels();
+            labels.forEach(label -> label.addTask(task));
+
+            taskRepository.save(task);
+            return taskMapper.map(task);
+
+        } catch (NoSuchElementException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+        }
     }
 
     public List<TaskDTO> getAll() {
@@ -37,6 +67,7 @@ public class TaskService {
     public List<TaskDTO> getAll(TaskFilterDTO taskFilterDTO) {
         var filter = taskSpecification.build(taskFilterDTO);
         var tasks = taskRepository.findAll(filter);
+
         return tasks.stream()
                 .map(taskMapper::map)
                 .toList();
@@ -49,15 +80,52 @@ public class TaskService {
     }
 
     public TaskDTO update(TaskUpdateDTO taskUpdateDTO, Long id) {
-        var task = taskRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Task With Id: " + id + " Not Found"));
-        taskMapper.update(taskUpdateDTO, task);
 
-        taskRepository.save(task);
-        return taskMapper.map(task);
+        try {
+            var task = taskRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Task With Id: " + id + " Not Found"));
+
+            taskMapper.update(taskUpdateDTO, task);
+
+            //записываю таску автору и автора таске:
+            var assignee = userRepository.findByEmail(task.getAssignee().getEmail()).orElseThrow();
+            assignee.addTask(task);
+
+            //записываю таску статусу и статус таске:
+            var status = taskStatusRepository.findBySlug(task.getTaskStatus().getSlug()).orElseThrow();
+            status.addTask(task);
+
+            //записываю таску в метки:
+            Set<Long> labelIds = task.getLabels().stream()
+                    .map(Label::getId)
+                    .collect(Collectors.toSet());
+
+            Set<Label> labels = labelRepository.findByIdIn(labelIds);
+            labels.forEach(label -> label.addTask(task));
+
+            taskRepository.save(task);
+            return taskMapper.map(task);
+
+        } catch (NoSuchElementException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+        }
     }
 
     public void delete(Long id) {
+
+        var task = taskRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Task with id " + id + " not found"));
+
+        //удаляю таску из списка автора:
+        task.getAssignee().removeTask(task);
+
+        //удаляю таску из статусов:
+        task.getTaskStatus().removeTask(task);
+
+        //удаляю таску из меток:
+        task.getLabels().forEach(label -> label.removeTask(task));
+
+        //удаляю таску из репо:
         taskRepository.deleteById(id);
     }
 }
