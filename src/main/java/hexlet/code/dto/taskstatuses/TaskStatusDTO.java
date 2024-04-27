e3c6b992276c021188ea921158a6cbc6bcf05110
alina tarasova <alishka.tarasova@gmail.com>
2024-04-27 14:32:36 +0300

    Updated TaskStatusDTO.java

--- src/main/java/hexlet/code/dto/taskstatuses/TaskStatusDTO.java
+++ src/main/java/hexlet/code/dto/taskstatuses/TaskStatusDTO.java
@@ -1,7 +1,9 @@
 package hexlet.code.dto.taskstatuses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -9,5 +11,7 @@ public class TaskStatusDTO {
     private long id;
     private String name;
     private String slug;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private String createdAt;
 }
