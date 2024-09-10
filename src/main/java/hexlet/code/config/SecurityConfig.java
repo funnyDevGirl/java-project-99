9fddb24595ce5b2cf55a6654ead2893e9d5d01a4
alina tarasova <alishka.tarasova@gmail.com>
2024-09-10 11:52:30 +0300

    Added html page with thymeleaf

--- src/main/java/hexlet/code/config/SecurityConfig.java
+++ src/main/java/hexlet/code/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package hexlet.code.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
--- src/main/java/hexlet/code/controller/MyFormController.java
+++ src/main/java/hexlet/code/controller/MyFormController.java
@@ -1,13 +1,48 @@
 package hexlet.code.controller;
 
+import hexlet.code.dto.users.UserCreateDTO;
+import hexlet.code.dto.users.UserDTO;
+import hexlet.code.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
 
 @Controller
+@AllArgsConstructor
 public class MyFormController {
+
+    private final UserService userService;
 
     @GetMapping("/myform")
     public String creating() {
-        return "creatingJS";
+//        //return "creatingJS"; // использование шаблона с JS
+        return "registration"; // использование шаблона с thymeleaf
+    }
+
+    @PostMapping("/myform")
+    public String processForm(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            Model model) {
+
+        // сохранение в БД:
+        UserCreateDTO userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setFirstName(firstName);
+        userCreateDTO.setLastName(lastName);
+        userCreateDTO.setEmail(email);
+        userCreateDTO.setPassword(password);
+
+        UserDTO createdUser = userService.create(userCreateDTO);
+
+        String fullName = createdUser.getFirstName() + " " + createdUser.getLastName();
+        model.addAttribute("fullName", fullName);
+
+        return "success";
     }
 }
